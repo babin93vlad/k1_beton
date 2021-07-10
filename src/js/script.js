9546a6cd8aa6cf7b6ffd5080678a6062850477cf
AlexRailean <aleks.railean@neurocorn.com>
2021-07-10 12:51:01 +0300

    Added language toggling

--- src/js/script.js
+++ src/js/script.js
@@ -5,6 +5,20 @@ window.addEventListener("DOMContentLoaded", () => {
 	const advantages = document.querySelector(".advantages");
 	const objectsArrow = document.querySelector(".objects__icon");
 	const advantagesArrow = document.querySelector(".advantages__icon");
+	const ruLangButton = document.querySelector(".nav__form.ru");
+	const roLangButton = document.querySelector(".nav__form.ro");
+
+	ruLangButton.addEventListener("click", () => {
+		localStorage.setItem("language", "RU");
+		$(".ru-text").fadeIn();
+		$(".ro-text").fadeOut();
+	});
+
+	roLangButton.addEventListener("click", () => {
+		localStorage.setItem("language", "RO");
+		$(".ro-text").fadeIn();
+		$(".ru-text").fadeOut();
+	});
 
 	objects.addEventListener("click", () => {
 		if (objectBlock.style.display === "block") {
@@ -40,6 +54,21 @@ window.addEventListener("DOMContentLoaded", () => {
 });
 
 $(document).ready(function () {
+	const ruLangButton = $("#ru");
+	const roLangButton = $("#ro");
+
+	if (localStorage.getItem("language") === "RU" || localStorage.getItem("language") == null) {
+		ruLangButton.prop("checked", true);
+		roLangButton.prop("checked", false);
+		$(".ru-text").fadeIn();
+		$(".ro-text").fadeOut();
+	} else {
+		roLangButton.prop("checked", true);
+		ruLangButton.prop("checked", false);
+		$(".ro-text").fadeIn();
+		$(".ru-text").fadeOut();
+	}
+
 	$(".carousel__inner").slick({
 		slidesToShow: 5,
 		slidesToScroll: 1,
